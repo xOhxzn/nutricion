1d870eaea055ee670ae30e095ba102d5cd92b3bf
Johan Rdz <joeljohan1717gmail.com>
2025-05-19 02:13:16 -0600

    Primer Commmit, se agrega el proyecto para subirlo a github

---
+++ src/main/java/com/appnutricion/NutricionApplication.java
@@ -0,0 +1,13 @@
+package com.appnutricion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutricionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NutricionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/appnutricion/NutricionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appnutricion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutricionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
